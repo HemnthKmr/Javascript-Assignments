f97b8517395f9b8edc03cad3478b70569b5798c7
Hemanth Kumar G <hemanthkmr@gmail.com>
2020-07-22 14:57:31 +0530

    Changes Made

--- [LU] JS Essentials Day 6 Assignment/scripts/Question2.js
+++ [LU] JS Essentials Day 6 Assignment/scripts/Question2.js
@@ -2,9 +2,10 @@
 let val = Number(prompt("Enter a number:"));
 let count = Number(prompt("Enter the number of iterations needed:"));
 
+list = document.querySelector('#list');
+
 for(i=1;i<=count;i++)
 {
-    list = document.querySelector('#list');
     mul = val * i;
     list.innerHTML += `<p> ${val} * ${i} = ${mul} </p>`
 }
--- [LU] JS Essentials Day 6 Assignment/scripts/Question3.js
+++ [LU] JS Essentials Day 6 Assignment/scripts/Question3.js
@@ -1,6 +1,42 @@
+// Welcome Message
+
 let input = prompt("Enter your name:");
 
 
 let change = document.getElementById('home');
 
 change.innerHTML = `Hi ${input}, Welcome to our page`;
+
+// Clock
+const ctime = document.getElementById('time');
+
+function clock(){
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    ctime.innerText = time;
+}
+
+setInterval(clock,1000);
+
+// DarkMode
+
+let darkmode = document.getElementById('Dark');
+let darkmode1 = document.getElementById('Darkbut');
+
+darkmode1.onclick = DarkMode = () => {
+    if (darkmode.getAttribute('Dark') != "black")
+    {
+        darkmode.classList.toggle('Darkmodeon');
+        
+    }
+    else{
+        darkmode.classList.toggle('Darkmodeoff');
+    }
+    
+}
+
+
+
+
+
+
