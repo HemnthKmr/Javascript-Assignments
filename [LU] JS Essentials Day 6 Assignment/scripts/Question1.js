887dfd229443bd8a21fa26cdd7630034090c2c9a
Hemanth Kumar G <hemanthkmr@gmail.com>
2020-07-25 23:04:35 +0530

    changes made

--- [LU] JS Essentials Day 6 Assignment/scripts/Question1.js
+++ [LU] JS Essentials Day 6 Assignment/scripts/Question1.js
@@ -1,4 +1,4 @@
-
+// Comment Question 3 for Question 1 to run.
 
 let i =0;
 
@@ -6,16 +6,15 @@ let i =0;
 setInterval( function backgroundChange(){
     colors = ["red","yellow","green","blue"];
     document.body.style.backgroundColor = `${colors[i]}`;
+
     if ( i < colors.length){
         i = i + 1;
     }
-    if(i == colors.length - 1)
+    if(i > colors.length - 1)
     {
         i = 0;
     }
-}, 2000);
-   
-    
+}, 5000);
 
     
 
